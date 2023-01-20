8d5a8bbe766d30aa218ec6f662dd2ea05e85e2d7
der-matt02 <105879241+der-matt02@users.noreply.github.com>
2023-01-20 14:03:27 -0500

    Update Colchonetas.c

--- Colchonetas.c
+++ Colchonetas.c
@@ -5,25 +5,40 @@ int main(){
     FILE *archivo=NULL;
     char buffer[100];
     double pi;
-    //char tardigrado[10] ="tardigrado";
 
-    archivo=fopen("multiploTardigrado.txt","w");
+    archivo=fopen("tardigrado.txt","r+");
     
     if (archivo==NULL){
         printf("No se puede abrir el archivo");
         return -1;
     }
 
-    for(int i=0; i<100; i++){
-        if(i % 10 == 0)
-        {
-            fprintf(archivo,"tardigrado\n");
-        }
-        else
-        {
-            fprintf(archivo,"%d\n",i);
-        }  
+    int cont=1;
+    while(!feof(archivo)){
+        fgets(buffer,100,archivo); //fgetc lee caracter por carater, junto con un for para que recorra la linea, fgets lee toda la linea
+        printf("la primera linea %i es %s",cont ,buffer);
+        cont++;
     }
-     fclose(archivo);
-     return 0;
+    
+/*
+    fgets(buffer,100,archivo); //fgetc lee caracter por carater, junto con un for para que recorra la linea, fgets lee toda la linea
+    printf("la primera linea es %s",buffer);
+
+    fgets(buffer,100,archivo); //lee la primera linea y deja mel cursor en la sgunda
+    printf("la segunda linea es %s",buffer);
+
+    fgets(buffer,100,archivo);
+    printf("la tercera linea es %s",buffer);
+
+    fscanf(archivo, "%lf", &pi); //tiene la capacida de leer datos fomatiados 
+    printf("el número en la cuarta linea es %lf",pi);
+*/
+    fprintf(archivo,"\n");
+
+    fputs("se agrega una quinta linea",archivo); //agrega limeas
+  
+    fclose(archivo);
+
+
+    return 0;
 }
